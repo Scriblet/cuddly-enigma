84c70b6f06952a455944580f3668de7648c4fdd5
Scriblet <46581168+Scriblet@users.noreply.github.com>
2019-06-01 21:36:43 -0300

    Update club_criando_bd_tabelas.sql

--- PROJETO AP2/Scripts/club_criando_bd_tabelas.sql
+++ PROJETO AP2/Scripts/club_criando_bd_tabelas.sql
@@ -200,6 +200,11 @@ CREATE TABLE associados_part_camp (
     CONSTRAINT fk_asso_camp
     FOREIGN KEY(id) 
     REFERENCES associado(id)
+	
+    CONSTRAINT fk_asso_camp
+    FOREIGN KEY (codigo_camp)
+    REFERENCES campeonato(cod_camp)
+	
 
 ) DEFAULT CHARSET = utf8mb4;
 
@@ -216,5 +221,23 @@ CREATE TABLE jogos (
     REFERENCES campeonato(cod_camp)
 ) DEFAULT CHARSET = utf8mb4;
 
+CREATE TABLE atualizacao_de_associados_camp(
+	id INT NOT NULL,
+	codigo_campeonato INT NOT NULL,
+	nome VARCHAR(15),
+	sobrenome VARCHAR(30),
+	data_de_atualizacao DATE,
+    
+	CONSTRAINT pk_historico_alunos
+	PRIMARY KEY(id, codigo_campeonato),
+
+	CONSTRAINT fk_historico_camp
+	FOREIGN KEY (codigo_campeonato)
+        REFERENCES campeonato(cod_camp),
+
+	CONSTRAINT fk_historico_asso
+	FOREIGN KEY (nome, sobrenome)
+        REFERENCES associado(nome, sobrenome)
+) DEFAULT CHARSET = utf8mb4;
 
 
