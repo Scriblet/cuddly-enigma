601551a33e1e930f1ed07f7225ed753e5f9799ea
Scriblet <46581168+Scriblet@users.noreply.github.com>
2019-06-05 00:11:04 -0300

    Update club_implementacoes.sql

--- PROJETO AP2/Scripts/club_implementacoes.sql
+++ PROJETO AP2/Scripts/club_implementacoes.sql
@@ -1,13 +1,5 @@
-CREATE DEFINER = CURRENT_USER TRIGGER `club_esportivo`.`associados_part_camp_BEFORE_UPDATE` BEFORE UPDATE ON `associados_part_camp` FOR EACH ROW -- trigger
-
-BEGIN
-
-	   
-    IF NEW.sobrenome <> OLD.sobrenome OR NEW.nome <> OLD.nome
-    
-    THEN
-        
-	
-	INSERT INTO atualizacao_de_associados_camp VALUES (NULL, NULL, NEW.name, NEW.sobrenome, NOW())
-    
-    END
+CREATE TRIGGER tr_nome_completo
+BEFORE INSERT
+ON associado
+FOR EACH ROW
+SET New.nome_completo = CONCAT( New.nome, ' ', New.sobrenome);
